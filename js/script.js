a9c770c96ae5fdad870c5e58a730646027c8a57d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-02 15:33:39 +0700

    init

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
         addTodo();
     });
 });
+
+function addTodo() {
+    const textTodo = document.getElementById('title').value;
+    const timestamp = document.getElementById('date').value;
+
+    const generatedID = generateId();
+    const todoObject = generateTodoObject(generatedID, textTodo, timestamp, false);
+    todos.push(todoObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
