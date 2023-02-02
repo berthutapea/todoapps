331c36be13f8bbb2a6c348779a7c676e71ae050b
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-02 15:31:31 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('form');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        addTodo();
+    });
+});
