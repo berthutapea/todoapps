1a3455e82451d60fa20839c894cd3f81c8039f40
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-02 16:28:22 +0700

    init

--- js/script.js
+++ js/script.js
@@ -34,6 +34,31 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
     }
 }
 
+function makeTodo(todoObject) {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    return container;
+}
+
 document.addEventListener(RENDER_EVENT, function () {
-    console.log(todos);
+    const uncompletedTODOList = document.getElementById('todos');
+    uncompletedTODOList.innerHTML = '';
+
+    for (const todoItem of todos) {
+        const todoElement = makeTodo(todoItem);
+        uncompletedTODOList.append(todoElement);
+    }
 });
